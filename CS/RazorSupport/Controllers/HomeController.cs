87cbd9d7ea200e3ad6661a4a9a7d520fbf21a5f3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RazorSupport/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RazorSupport.Models;
+
+namespace RazorSupport.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return GridView();
+        }
+        public ActionResult GridView() {
+            return View("GridView", NorthwindDataProvider.GetInvoices());
+        }
+        public ActionResult GridViewPartial() {
+            return PartialView("GridViewPartial", NorthwindDataProvider.GetInvoices());
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use the Razor view engine with DevExpress Extensions for ASP.NET MVC
+
+
+<p>The example demonstrates how to use the Razor engine with MVC extensions.</p>
+
+<br/>
+
+
