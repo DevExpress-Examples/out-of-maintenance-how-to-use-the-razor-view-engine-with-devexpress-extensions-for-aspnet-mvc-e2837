7a8fad7a049a67b5fafc979546980f71ebe8920b
DevExpressExampleBot <fakeEmail>
2020-12-28 15:32:12 +0300

    Source auto update [skip ci]

---
+++ VB/RazorSupport/Controllers/HomeController.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+Imports RazorSupport.Models
+
+Namespace RazorSupport.Controllers
+	Public Class HomeController
+		Inherits Controller
+
+		Public Function Index() As ActionResult
+			Return GridView()
+		End Function
+		Public Function GridView() As ActionResult
+			Return View("GridView", NorthwindDataProvider.GetInvoices())
+		End Function
+		Public Function GridViewPartial() As ActionResult
+			Return PartialView("GridViewPartial", NorthwindDataProvider.GetInvoices())
+		End Function
+	End Class
+End Namespace
---
+++ VB/RazorSupport/Models/Northwind.vb
@@ -0,0 +1,24 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Collections
+
+Namespace RazorSupport.Models
+	Public Module NorthwindDataProvider
+'INSTANT VB NOTE: The field db was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private db_Conflict As NorthwindDataContext
+		Public ReadOnly Property DB() As NorthwindDataContext
+			Get
+				If db_Conflict Is Nothing Then
+					db_Conflict = New NorthwindDataContext()
+				End If
+				Return db_Conflict
+			End Get
+		End Property
+		Public Function GetInvoices() As IEnumerable
+			Return From invoice In DB.Invoices
+				Select invoice
+		End Function
+	End Module
+End Namespace
